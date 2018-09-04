b2d9be1b9f7a256db0044b84823b694a9fdfc327
lyc <hellocworld@naver.com>
2018-09-04 17:23:10 +0900

    v0.2

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
 
 public class App {
-    
-    
+    public static void main(String[] args) {
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] passwords = new String[100];
+        
+        int index = 0;
+        
+        Scanner keyIn = new Scanner(System.in);
+        
+        
+        while (true) {
+            System.out.print("이름? ");
+            names[index] = keyIn.nextLine();
+            
+            System.out.print("이메일? ");
+            emails[index] = keyIn.nextLine();
+            
+            System.out.print("암호? ");
+            passwords[index] = keyIn.nextLine();
+            
+            index++;
+            
+            System.out.print("계속 하시겠습니까?(Y/n) ");
+            String answer = keyIn.nextLine();
+            if (answer.toLowerCase().equals("n"))
+                break;
+        }
+        
+        for (int i = 0; i < index; i++) {
+            System.out.printf("%s, %s, %s\n", 
+                    names[i], emails[i], passwords[i]);
+        }
+        
+        keyIn.close();
+    }
 }
