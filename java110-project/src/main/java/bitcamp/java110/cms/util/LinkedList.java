4935259c9b56de2cee9ca67c64ed707691c59cd2
lyc <hellocworld@naver.com>
2018-09-06 14:41:47 +0900

    LinkedList 생성

--- java110-project/src/main/java/bitcamp/java110/cms/util/LinkedList.java
+++ java110-project/src/main/java/bitcamp/java110/cms/util/LinkedList.java
@@ -31,12 +31,45 @@ public class LinkedList <T>{
         return (T)cursor.value;
     }
     
-    public T remove(int index) {
-        return null;
+    public T remove(int index) {//손코딩해라~
+        if(index < 0 || index >= length)
+            return null;
+        length--;
+        Node<T> cursor=first;
+        for(int count=0;count < index; count++) {
+            cursor=cursor.next; //일단 삭제하려는 노드를 찾고
+        }
+        if(cursor==first) {//첫번쨰 노드 지우기
+            first=first.next;
+            first.prev=null;
+            return cursor.value;//보통 지우고 지운 값을 리턴해준다~
+        }
+        
+        cursor.prev.next=cursor.next;
+        cursor.next.prev=cursor.prev;
+        return cursor.value;
     }
     
     public void insert(int index, T obj) {
+        if(index < 0 || index >= length)
+            return;
+        length++;
         
+        Node<T> node=new Node<>();
+        node.value=obj;
+        
+        Node<T> cursor=first;
+        for(int count=0;count < index; count++) {
+            cursor=cursor.next;
+        }
+        //이전 노드가 있어야만 이전 노드를 새 노드에 연결할 수 있다.
+        if(cursor != first) {
+            cursor.prev.next=node;//이전 노드가 새 노드를 가리킨다
+            node.prev=cursor.prev;//새 노드는 이전 노드를 가리킨다.
+        }
+        
+        cursor.prev=node;//현재 노드의 이전은 새 노드를 가리킨다.
+        node.next=cursor;//새 노드의 다음은 현재노드
     }
     
     public int size() {
