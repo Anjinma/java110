1ab0983f996f27bfb6bf230baf43855615f715eb
lyc <hellocworld@naver.com>
2018-09-17 12:09:28 +0900

    소켓 프로그래밍에 맞춰 컨트롤러 변경

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -3,7 +3,7 @@ package bitcamp.java110.cms.server;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,12 +12,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
 
-public class ServerApp  {
-
+public class ServerApp {
+    
     ClassPathXmlApplicationContext iocContainer;
     RequestMappingHandlerMapping requestHandlerMap;
-
-    public ServerApp() throws Exception{
+    
+    public ServerApp() throws Exception {
         createIoCContainer();
         logBeansOfContainer();
         processRequestMappingAnnotation();
@@ -46,31 +46,41 @@ public class ServerApp  {
         System.out.println("------------------------");
     }
     
-    public void service() throws Exception{
-        //클라이언트 연결을 기다리는 서버 소켓 준비
-        ServerSocket serverSocket =  new ServerSocket(8888);
+    public void service() throws Exception {
+        // 클라이언트 연결을 기다리는 서버 소켓 준비
+        ServerSocket serverSocket = new ServerSocket(8888);
         System.out.println("서버 실행 중...");
-
-        while(true) {
-            try(
+        
+        while (true) {
+            try (
                     Socket socket = serverSocket.accept();
-                    PrintStream out = new PrintStream(new BufferedOutputStream(socket.getOutputStream()));
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    ){
+                    PrintWriter out = new PrintWriter(
+                            new BufferedOutputStream(
+                                    socket.getOutputStream()));
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(
+                                    socket.getInputStream()));
+                    ) {
                 System.out.println(in.readLine());
-                out.println("OK: 용찬");  out.flush();
+                out.println("OK:강사"); out.flush();
                 
-                while(true) {
-                    String requestLine =  in.readLine();
-                    
-                    if(requestLine.equals("EXIT")) {
-                        out.println("GoodBye!");
+                while (true) {
+                    String requestLine = in.readLine();
+                    if (requestLine.equals("EXIT")) {
+                        out.println("goodbye");
                         out.println();
                         out.flush();
                         break;
                     }
                     
-                    RequestMappingHandler mapping = requestHandlerMap.getMapping(requestLine);
+                    // 요청 객체 준비
+                    Request request = new Request(requestLine);
+                    
+                    // 응답 객체 준비
+                    Response response = new Response(out);
+
+                    RequestMappingHandler mapping = 
+                            requestHandlerMap.getMapping(request.getAppPath());
                     if (mapping == null) {
                         out.println("해당 요청을 처리할 수 없습니다.");
                         out.println();
@@ -79,14 +89,17 @@ public class ServerApp  {
                     }
                     
                     try {
-                        mapping.getMethod().invoke(mapping.getInstance(), out);
+                        // 요청 핸들러 호출
+                        mapping.getMethod().invoke(mapping.getInstance(), request, response);
+                        
                     } catch (Exception e) {
-                        e.printStackTrace();//서버쪽에 오류 알림
-                        out.println("요청 처리 중에 요류가 밠행했습니다.");//클라이언트쪽에 오류 알림
+                        e.printStackTrace();
+                        out.println("요청 처리 중에 오류가 발생했습니다.");
                     }
                     out.println();
                     out.flush();
                 }
+                
             }
         }
     }
