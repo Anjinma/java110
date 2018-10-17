9a072a10c20a3173550835994a54c441cc89edbf
lyc <hellocworld@naver.com>
2018-10-17 09:16:28 +0900

    mybatis를 이용하여 DAO 구현체 제거하기, 트랜잭션적용

--- java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
+++ java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
@@ -1,20 +1,19 @@
 package bitcamp.java110.cms.listener;
 
+import java.io.InputStream;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import bitcamp.java110.cms.dao.impl.ManagerMysqlDao;
-import bitcamp.java110.cms.dao.impl.MemberMysqlDao;
-import bitcamp.java110.cms.dao.impl.PhotoMysqlDao;
-import bitcamp.java110.cms.dao.impl.StudentMysqlDao;
-import bitcamp.java110.cms.dao.impl.TeacherMysqlDao;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import bitcamp.java110.cms.service.impl.AuthServiceImpl;
 import bitcamp.java110.cms.service.impl.ManagerServiceImpl;
 import bitcamp.java110.cms.service.impl.StudentServiceImpl;
 import bitcamp.java110.cms.service.impl.TeacherServiceImpl;
-import bitcamp.java110.cms.util.DataSource;
-import bitcamp.java110.cms.util.TransactionManager;
 
 //@WebListener
 public class ContextLoaderListener implements ServletContextListener {
@@ -24,54 +23,24 @@ public class ContextLoaderListener implements ServletContextListener {
         
         ServletContext sc = sce.getServletContext();
 
-        // DAO가 사용할 DB 커넥션풀 객체 준비
-        // => DataSource 객체를 만들 때 컨텍스트 파라미터 값을 꺼내서 사용한다.
         try {
-            DataSource dataSource = new DataSource(
-                    sc.getInitParameter("jdbc.driver"),
-                    sc.getInitParameter("jdbc.url"),
-                    sc.getInitParameter("jdbc.username"),
-                    sc.getInitParameter("jdbc.password"));
-            
-            TransactionManager txManager = TransactionManager.getInstance();
-            txManager.setDataSource(dataSource);
-            
-            // DAO 객체 생성 및 DB 커네션풀 주입하기
-            MemberMysqlDao memberDao = new MemberMysqlDao();
-            memberDao.setDataSource(dataSource);
-            
-            ManagerMysqlDao managerDao = new ManagerMysqlDao();
-            managerDao.setDataSource(dataSource);
-            
-            StudentMysqlDao studentDao = new StudentMysqlDao();
-            studentDao.setDataSource(dataSource);
-            
-            TeacherMysqlDao teacherDao = new TeacherMysqlDao();
-            teacherDao.setDataSource(dataSource);
-            
-            PhotoMysqlDao photoDao = new PhotoMysqlDao();
-            photoDao.setDataSource(dataSource);
+            String resource = "bitcamp/java110/cms/conf/mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory =
+              new SqlSessionFactoryBuilder().build(inputStream);
             
             // 서비스 객체 준비하기
             ManagerServiceImpl managerService = new ManagerServiceImpl();
-            managerService.setMemberDao(memberDao);
-            managerService.setManagerDao(managerDao);
-            managerService.setPhotoDao(photoDao);
+            managerService.setSqlSessionFactory(sqlSessionFactory);
             
             StudentServiceImpl studentService = new StudentServiceImpl();
-            studentService.setMemberDao(memberDao);
-            studentService.setStudentDao(studentDao);
-            studentService.setPhotoDao(photoDao);
+            studentService.setSqlSessionFactory(sqlSessionFactory);
             
             TeacherServiceImpl teacherService = new TeacherServiceImpl();
-            teacherService.setMemberDao(memberDao);
-            teacherService.setTeacherDao(teacherDao);
-            teacherService.setPhotoDao(photoDao);
+            teacherService.setSqlSessionFactory(sqlSessionFactory);
             
             AuthServiceImpl authService = new AuthServiceImpl();
-            authService.setManagerDao(managerDao);
-            authService.setStudentDao(studentDao);
-            authService.setTeacherDao(teacherDao);
+            authService.setSqlSessionFactory(sqlSessionFactory);
             
             // 서블릿에서 Service를 이용할 수 있도록 ServletContext 보관소에 저장하기
             sc.setAttribute("managerService", managerService);
